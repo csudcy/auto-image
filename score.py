0a6b3fbedf4a4591be8e41b117b4d8e820d69e33
Nick <csudcy@gmail.com>
2024-10-27 10:48:14 +0000

    Show stats when processing finishes

--- score.py
+++ score.py
@@ -23,10 +23,19 @@ def _save_scores(scores: dict) -> None:
   os.replace(temp_file.name, utils.SCORE_FILE)
 
 
-print('Predicting...')
-SCORES = utils.load_scores()
 processed = 0
 start_time = time.perf_counter()
+def _show_stats():
+  diff_time = time.perf_counter() - start_time
+  if processed:
+    processed_per_minute = processed / (diff_time / 60)
+  else:
+    processed_per_minute = 0
+  print(f'Done {index} files (scored {processed} in {diff_time:.02f}s, {processed_per_minute:.02f} per minute)')
+
+
+print('Predicting...')
+SCORES = utils.load_scores()
 next_time = start_time
 for index, path in enumerate(utils.IMAGE_FOLDER.rglob('*')):
   if IMAGE_LIMIT and index >= IMAGE_LIMIT:
@@ -34,14 +43,8 @@ for index, path in enumerate(utils.IMAGE_FOLDER.rglob('*')):
     break
 
   if time.perf_counter() >= next_time:
-    current_time = time.perf_counter()
-    diff_time = current_time - start_time
-    if processed:
-      processed_per_minute = processed / (diff_time / 60)
-    else:
-      processed_per_minute = 0
-    print(f'Done {index} files (scored {processed} in {diff_time:.02f}s, {processed_per_minute:.02f} per minute)')
-    next_time = current_time + 5
+    _show_stats()
+    next_time = time.perf_counter() + 5
     _save_scores(SCORES)
 
   # Check if the file has been scored already
@@ -63,6 +66,7 @@ for index, path in enumerate(utils.IMAGE_FOLDER.rglob('*')):
   # Save results back to scores
   SCORES[file_id] = file_scores
 
+_show_stats()
 _save_scores(SCORES)
 
 print('Done!')
--- score_musiq.py
+++ score_musiq.py
@@ -1,3 +1,5 @@
+# Based on https://github.com/google-research/google-research/tree/master/musiq
+
 import tensorflow_hub as hub
 
 MODEL = None
@@ -6,8 +8,8 @@ MODEL = None
 def get_score(image_bytes: bytes) -> float:
   global MODEL
   if MODEL is None:
-    # https://github.com/google-research/google-research/tree/master/musiq
     MODEL = hub.load('https://www.kaggle.com/models/google/musiq/TensorFlow2/ava/1')
+
   predict = MODEL.signatures['serving_default']
   prediction = predict(image_bytes)
   return float(prediction['output_0'].numpy())
