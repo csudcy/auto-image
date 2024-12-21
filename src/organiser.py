261d238af0ebcfef6a3299ea925a211985fd35b3
Nick <csudcy@gmail.com>
2024-12-21 12:54:39 +0000

    Add location to cropped images

--- src/organiser.py
+++ src/organiser.py
@@ -3,6 +3,8 @@ import shutil
 from typing import Optional
 
 from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 from PIL import ImageOps
 
 from src import result_manager
@@ -13,7 +15,22 @@ def process(
     output_folder: pathlib.Path,
     apply: bool,
     crop_size: Optional[tuple[int, int]],
+    font_size: int = 50,
+    font_filename: str = 'Tahoma.ttf',
+    text_offset_x: int = 20,
+    text_offset_y: int = -100,
+    font_colour: tuple[int, int, int] = (255, 255, 255),
+    font_outline_width: int = 3,
+    font_outline_colour: tuple[int, int, int] = (0, 0, 0),
 ) -> None:
+  font = ImageFont.truetype(font_filename, font_size)
+  outline_offsets = [
+      (x_offset, y_offset)
+      for x_offset in range(-font_outline_width, font_outline_width+1)
+      for y_offset in range(-font_outline_width, font_outline_width+1)
+      if (x_offset, y_offset) != (0, 0)
+  ]
+
   print('Checking files...')
   # Find all files in the target folder
   output_folder.mkdir(parents=True, exist_ok=True)
@@ -56,6 +73,15 @@ def process(
         else:
           centre = (0.5, 0.5)
         cropped = ImageOps.fit(image, crop_size, centering=centre)
+        # Add location
+        if result.location:
+          draw = ImageDraw.Draw(cropped)
+          text_y = crop_size[1] + text_offset_y
+          # Outline
+          for x, y in outline_offsets:
+            draw.text((text_offset_x + x, text_y + y), result.location, font_outline_colour, font=font)
+          # Text
+          draw.text((text_offset_x, text_y), result.location, font_colour, font=font)
         cropped.save(output_path, quality=95)
       else:
         shutil.copy(result.path, output_path)
