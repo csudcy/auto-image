3bf1e71e458007309366194f002c96e6ac38529e
Nick <csudcy@gmail.com>
2025-02-22 19:54:20 +0000

    Remove base script

--- scripts/score_dropbox_base.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Score dropbox images
-
-./scripts/poetry_run.sh python3 score.py \
-  "/Users/csudcy/Library/CloudStorage/Dropbox/Camera Uploads" \
-  --output-count=5000 \
-  "${@}"
